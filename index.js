1d50966816b3b540f0df816e8b1ebac6d1506105
Jarin Simon <jarin.simon@gmail.com>
2023-08-29 12:30:19 -0700

    Functionality to input number of players in league

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config()
 const express = require("express");
 var request = require("request");
+const prompt = require('prompt-sync')();
 const fs = require("fs");
+const readLine = require("readline")
+const { resolve } = require("path");
 const app = express();
 const apiKey = process.env.sports_key
 const path = './config.json';
@@ -16,25 +19,58 @@ var options = {
     }
 };
 
-fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-        console.error("Error reading JSON file:", err);
-        return;
-    }
+var chosenTeam;
+var numPlayers;
+var perTeam;
 
-    const jsonData = JSON.parse(data);
-    const idToName = {};
+function getNumPlayers() {
+    return new Promise(resolve => {
+        const rl = readLine.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('How many players are in your pickem league?\n', (answer) => {
+            resolve(answer)
+            perTeam = Math.floor(32/answer);
+            const remainder = 32 % perTeam;
 
-    for (const response of jsonData.response){
-        const teamId = response.team.id;
-        const teamName = response.team.name;
-        idToName[teamId] = teamName;
-    }
-    for (let i = 1; i <= 32; i++){
-        const teamName = idToName[i];
-        console.log(i, teamName);
-    }
-});
+            console.log(`You have ${answer} players in your Pickem League\nEach player should have ${perTeam} teams, with ${remainder} teams unpicked`);
+            rl.close();
+        })
+    })
+}
+
+async function getPlayers(){
+    numPlayers = await getNumPlayers();
+    fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            return;
+        }
+    
+        const jsonData = JSON.parse(data);
+        const idToName = {};
+        const NameToId = {}
+    
+        for (const response of jsonData.response){
+            const teamId = response.team.id;
+            const teamName = response.team.name;
+            idToName[teamId] = teamName;
+            NameToId[teamName] = teamId;
+        }
+        var k = 1;
+        for (let i = 1; i <= numPlayers; i++){
+            for (let j = 1; j <= perTeam; j++){
+                console.log(`Player ${i} Team ${j}:`)
+                console.log(idToName[k] + '\n');
+                k++;
+            }
+        }
+    });
+}
+
+getPlayers();
+
 // request(options, (error, response, body) => {
 //     if (!error && response.statusCode === 200){
 //         try {
